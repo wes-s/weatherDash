97afafecedbf0999f1e3335aa9b154d33de6b172
Wes <wscogg2@wgu.edu>
2019-08-24 18:15:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ class getChart(Resource):
         temp_c.columns = locations
         colors = viridis(len(locations*2))
 
-        p = figure(width=1000, height=600, x_axis_type="datetime")
+        p = figure(width=800, height=500, x_axis_type="datetime")
 
         for num, location in enumerate(locations, start=0):
             p.line(temp_c.index.values, temp_c[0:][location], legend=location, color=colors[num*2+1], line_width=4)
