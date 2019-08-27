3b0b6dc19b79b1ae6f3e272b2d45f5402a4d084b
wsco <wwscoggin@gmail.com>
2019-08-27 08:45:15 -0700

    update app.py to weatherbit.io api

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask, request, render_template, make_response
-from flask_restful import Api, Resource, reqparse
-import requests
+from flask_restful import Api, Resource
 from flask import send_file
 # from pyowm import OWM
 from weatherbit.api import Api as wApi
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
 import pytz
 import pandas as pd
-from bokeh.palettes import Spectral, viridis, magma, plasma
-from bokeh.plotting import figure, show, output_notebook
-from bokeh.io import export_png
-from bokeh.resources import CDN
+from bokeh.palettes import plasma
+from bokeh.plotting import figure, show
 from bokeh.embed import components
 from bokeh.models import DatetimeTickFormatter
 import math
@@ -29,79 +26,60 @@ class getChart(Resource):
         wapi.set_granularity('hourly')
 
         locations = locations.split(';')
-        # owm = OWM(key)
-        # def getForecastfromDict(loc):
-        #     obs = owm.weather_at_place(loc)                    # Toponym
-        #     w = obs.get_weather()
-        #     curr_hour = datetime.fromtimestamp(w.get_reference_time())
-        #     dict = {}
-        #     dict.update({curr_hour.replace(second=0, microsecond=0, minute=0, hour=curr_hour.hour)+timedelta(hours=curr_hour.minute//30):
-        #                  {
-        #                      'location': loc
-        #                      ,'timestring':datetime.fromtimestamp(w.get_reference_time()).strftime('%Y-%m-%d %X')
-        #                      ,'status':w.get_status()
-        #                      ,'detstat':w.get_detailed_status()
-        #                      ,'windspeed':w.get_wind()['speed']
-        # #                      ,'winddirection':w.get_wind()['deg']
-        #                      ,'temp':w.get_temperature('fahrenheit')['temp']
-        #                      ,'temp_max':w.get_temperature('fahrenheit')['temp_max']
-        #                      ,'temp_min':w.get_temperature('fahrenheit')['temp_min']
-        #                      }
-        #                     })
-        #     fc = owm.three_hours_forecast(loc)
-        #     f = fc.get_forecast()
-        #     fw =f.get_weathers()
-        #     for weather in fw:
-        #         timestring = datetime.fromtimestamp(weather.get_reference_time()).strftime('%Y-%m-%d %X')
-        #         dict.update({datetime.fromtimestamp(weather.get_reference_time()):
-        #                      {
-        #                           'location': loc
-        #                          ,'timestring': timestring
-        #                          ,'status':weather.get_status()
-        #                          ,'detstat':weather.get_detailed_status()
-        #                          ,'windspeed':weather.get_wind()['speed']
-        # #                          ,'winddirection':weather.get_wind()['deg']
-        #                          ,'temp':weather.get_temperature('fahrenheit')['temp']
-        #                          ,'temp_max':weather.get_temperature('fahrenheit')['temp_max']
-        #                          ,'temp_min':weather.get_temperature('fahrenheit')['temp_min']
-        #                      }
-        #                     })
-        #     df = pd.DataFrame.from_dict(dict, orient='index')
-        #     return df
+        temps_h = []
+        temps_d = []
 
-        temps = []
-        for location in locations:
+        def getForecastFromWeatherBit(location, granularity):
+            wapi.set_granularity(granularity)
             forecast = wapi.get_forecast(city=location, units='I')
-            f = forecast.get_series(['temp'])
-            dict = {}
-            for temp in f:
-                dict.update({datetime.fromtimestamp(datetime.timestamp(pytz.utc.localize(temp['datetime']))):
-                 {
-                     'temp': temp['temp']
-                 }
+            if granularity == 'hourly':
+                att = 'temp'
+            else:
+                att = 'max_temp'
+            t = forecast.get_series([att])
+            dict_t = {}
+            for temp in t:
+                if granularity == 'hourly':
+                    idx = datetime.fromtimestamp(datetime.timestamp(pytz.utc.localize(temp['datetime'])))
+                else:
+                    idx = datetime.fromtimestamp(datetime.timestamp(pytz.utc.localize(temp['datetime']))).replace(second=0, microsecond=0, minute=0, hour=temps_h[-1].iloc[-1].name.hour+2)
+                dict_t.update({idx:
+                    {
+                        'temp': temp[att]
+                    }
                 })
-            temps.append(pd.DataFrame.from_dict(dict, orient='index'))
-        # for location in locations:
-        #     temps.append(getForecastfromDict(location)[['temp']])
+            df = pd.DataFrame.from_dict(dict_t, orient='index')
+            return df
 
-        temp_c = pd.concat(temps,axis=1)
-        temp_c.columns = locations
+        for location in locations:
+            temps_h.append(getForecastFromWeatherBit(location,'hourly'))
+        #     print(temps_h[-1].iloc[-1].name)
+            temps_d.append(getForecastFromWeatherBit(location,'daily')[3:7])
+
+        tempH = pd.concat(temps_h,axis=1)
+        tempH.columns = locations
+
+        tempD = pd.concat(temps_d,axis=1)
+        tempD.columns = locations
+
         colors = plasma(len(locations*2))
 
         p = figure(width=950, height=600, x_axis_type="datetime")
 
         for num, location in enumerate(locations, start=0):
-            p.line(temp_c.index.values, temp_c[0:][location], legend=location, color=colors[num*2+1], line_width=4)
-            p.circle(temp_c.index.values, temp_c[0:][location],  color=colors[num*2],size=8)
+            p.line(tempH.index.values, tempH[0:][location], legend=location, color=colors[num*2+1], line_width=5)
+            p.circle(tempH.index.values, tempH[0:][location],  color=colors[num*2],size=8)
+            p.line(tempD.index.values, tempD[0:][location], legend=location, color=colors[num*2+1], line_dash=[10,2], line_width=3)
+            p.circle(tempD.index.values, tempD[0:][location],  color=colors[num*2],size=8)
         
         p.xaxis.formatter=DatetimeTickFormatter(
-               hours=["%H:00"],
-               days=["%m-%d %H:00"],
-               months=["%m-%d %H:00"],
-               years=["%m-%d %H:00"]
-           )
+                hours=["%I:00 %p"],
+                days=["%A %m-%d"],
+                months=["%A %m-%d"],
+                years=["%A %m-%d"]
+        )
         
-        p.xaxis[0].ticker.desired_num_ticks = len(temp_c)*2
+        p.xaxis[0].ticker.desired_num_ticks = len(tempH)
 
         p.xaxis.major_label_orientation = math.pi/4
 
